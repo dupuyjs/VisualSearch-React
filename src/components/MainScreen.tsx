1a7e5a8b3bdec5b398d63fc985dea794d1418220
Jean-Sébastien Dupuy <jdupuy@microsoft.com>
2019-01-21 16:29:19 +0100

    Cleaning MainScreen

--- src/components/MainScreen.tsx
+++ src/components/MainScreen.tsx
@@ -1,56 +1,48 @@
-import * as React from 'react'
-import { Button } from 'react-native'
-import { Component } from 'react'
-import { NavigationScreenProps } from 'react-navigation'
-import { ScrollView } from 'react-native'
+import * as React from 'react';
+import { Component } from 'react';
+import { Button, ScrollView } from 'react-native';
+import { NavigationScreenProps } from 'react-navigation';
 
 class DestinationAndTitle {
-    constructor(
-        destination: string,
-        title?: string
-    ) {
-        this.destination = destination
+  constructor(destination: string, title?: string) {
+    this.destination = destination;
 
-        if (title === undefined) {
-            this.title = destination
-        }
-        else {
-            this.title = title
-        }
+    if (title === undefined) {
+      this.title = destination;
+    } else {
+      this.title = title;
     }
+  }
 
-    public destination: string
-    public title: string
+  public destination: string;
+  public title: string;
 }
 
-// tslint:disable-next-line:max-classes-per-file
 export class MainScreen extends Component<NavigationScreenProps> {
-    public static navigationOptions = {
-        title: 'Home'
-    }
+  public static navigationOptions = {
+    title: 'Home',
+  };
 
-    public render() {
-        return (
-            <ScrollView
-                style={{
-                    backgroundColor: '#f4f4f4',
-                    flex: 1
-                }}
-            >
-                {this.destinationAndTitlePairs.map(destinationAndTitle =>
-                    <Button
-                        key={destinationAndTitle.destination}
-                        onPress={() => this.props.navigation.navigate(destinationAndTitle.destination)}
-                        title={destinationAndTitle.title}
-                    />
-                )}
-            </ScrollView>
-        )
-    }
+  public render() {
+    return (
+      <ScrollView
+        style={{
+          backgroundColor: '#f4f4f4',
+          flex: 1,
+        }}
+      >
+        {this.destinationAndTitlePairs.map((destinationAndTitle) => (
+          <Button
+            key={destinationAndTitle.destination}
+            onPress={() => this.props.navigation.navigate(destinationAndTitle.destination)}
+            title={destinationAndTitle.title}
+          />
+        ))}
+      </ScrollView>
+    );
+  }
 
-    private destinationAndTitlePairs: Array<DestinationAndTitle> = [
-        new DestinationAndTitle('FetchVisualSearch', 'Bing Visual Search sample'),
-        // new DestinationAndTitle('StartWorking'),
-        // new DestinationAndTitle('CameraScreen')
-    ]
+  private destinationAndTitlePairs: DestinationAndTitle[] = [
+    new DestinationAndTitle('CameraScreen', 'Bing Custom Visual Search'),
+  ];
 }
