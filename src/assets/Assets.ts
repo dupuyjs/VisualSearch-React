39493ec996ed0a1c1889bae460f01422c18cb91d
Jean-Sébastien Dupuy <jdupuy@microsoft.com>
2019-01-21 16:27:10 +0100

    Adding Assets

---
+++ src/assets/Assets.ts
@@ -0,0 +1,11 @@
+export class Assets {
+  static Back = require('../assets/chevron-left.png');
+  static Camera = require('../assets/camera.png');
+  static Check = require('../assets/check.png');
+  static Crop = require('../assets/crop.png');
+  static Flash = require('../assets/flash.png');
+  static FlashOff = require('../assets/flash-off.png');
+  static FlashAuto = require('../assets/flash-auto.png');
+  static Image = require('../assets/image.png');
+  static Reload = require('../assets/reload.png');
+}
